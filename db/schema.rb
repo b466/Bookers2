7520f87067bfe8dc0d4678ed7c4e67a11f7580f6
、健太 <kenta@kentanoMacBook-Pro.local>
2020-03-05 19:25:44 +0900

    [remove] products

--- db/schema.rb
+++ db/schema.rb
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 2020_02_23_144532) do
     t.integer "user_id"
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "titlee"
-    t.integer "bodye"
-    t.text "detail"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
